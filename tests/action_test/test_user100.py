d6aab10fbb671f0f4ed138bd1c1e30eb89c1fd94
linux2017 <dangerousfeng@gmail.com>
2018-03-06 10:03:35 +0800

    peewee version 3.*-->2.*

--- tests/action_test/test_user100.py
+++ tests/action_test/test_user100.py
@@ -10,13 +10,17 @@ from tests.action_test.sanic_post import post_http
 
 data_100 = {
         "JWT": "%.24f" % random.random(),
-        "email": "1103@qq.com",
+        "email": "110fssdad2sd3@qq.com",
         "password": "123456",
         "superRole": True,
         "ActionId": 100}
 
-
+data_101 = {
+        "JWT": "%.24f" % random.random(),
+        "email": "qwer@qq.com",
+        "password": "123456",
+        "ActionId": 101}
 
 if __name__=='__main__':
-    res = post_http(data_100)
+    res = post_http(data_101)
     print(res)
